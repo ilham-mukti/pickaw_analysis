273dd73f2bb68b127e4dafc02be5a45e1ae500cb
Ilham Mukti <72337230+ilham-mukti@users.noreply.github.com>
2022-10-15 12:46:59 +0700

    update headers cookies

--- pickaw_analysis.py
+++ pickaw_analysis.py
@@ -13,22 +13,22 @@ class PickawContest:
     self.seed_list = "seed_list.json"
     self.cookies = {
         'i18n-lang': 'en',
-        '_ga': 'GA1.2.908142620.1664860279',
-        '_gid': 'GA1.2.1394393021.1664860279',
-        '_fbp': 'fb.1.1664860279910.400619248',
-        'crisp-client%2Fsession%2Fad02dfcf-7460-460d-bc6d-658345899d51': 'session_10a4289d-c94f-473e-9d0a-8a690793e675',
+        '_ga': 'GA1.2.367684644.1665811144',
+        '_gid': 'GA1.2.1270628231.1665811144',
+        '_fbp': 'fb.1.1665811144465.357421690',
+        'crisp-client%2Fsession%2Fad02dfcf-7460-460d-bc6d-658345899d51': 'session_3d39dea8-bd1d-40ec-8e21-43da153e1c1a',
         'crisp-client%2Fsocket%2Fad02dfcf-7460-460d-bc6d-658345899d51': '1',
+        'registration': '13671c4f940343259887329f34792118sq7uT7DbNgNBvqerfyquWJa86%2BjY6yi7yYzFpgWKy4BAwGccR%2FxNMisNRREkKfyVCDp8Xl3qSf3LGZeG4AWBPKL%2BfOxboYnl8sSgHBqLOM01rzdMYkeD1Y8dldXCzG%2F3FRxaFan%2FWB2E8%2FJJc%2FGpw%2BPle%2F0fzrWih0%2FTne5dPJ1zrSgW0CNpxHcrbogGz7DbgUKRqln3flM%2BgF%2FxlnYV%2BLdO1DqjLmyZyKb7oWgHBAV0dM2dZX6MW1gdUgFHT3gcx8Nv8L1%2BLwZ85M1SDE9KZ5rY05lidL6YJLBnCBZ8aL19SPX1mpOVMIbtz%2BWjyp0Xz%2FdMzA3sRrrEjnV7cP4j57Wkm%2F1A9AxwHY4zZXQMkNzLoZPtKlPslyYSHJJVNr8kUjfHDV4jXunuslwcLQmij4KJXPGikpojshoFWzSt3GReTVmYodQEhrC0fIw2d3%2B3O8LKIvJQD%2FXEvh0TvDtMJZP4VzW0PByANQLkoc75H9Q5YpTO3XCxKKFkWs5qdq7lrAiyjHnzgNWv6ycWN5Qo4rmK5gyWpUFD165eOXPYz3%2BTkLRXqBrNcISscW4gomVHKFpigWGtE89jHgxgYjfw%2FpNU00bdfUhGgsfIkAnTbL2C6ZShiGUD4Hs1DEGTHBV2nQEMHi9iYwJGrdDvrMKJwCqZEbYVpDF%2B4LQebMqzhuDMqN8WZjpmYZ3k5F8QZ1orBHAk%2F5tdJsXjETJ%2FpzybeRd1ysz4jpWZlDb6MH%2FhFbFXIGIEJElpLe26Wj2tAR47pWoae4soJ13I9%2BAFhGKUY79yf5zYygG4DfoBZpiLRVVw8ykygyKvyaF0%2BLau5CHsvcD46v66hDFKN1dx88Dqag%3D%3D',
+        'session': '5b52e8745adcc290c412d72f4686a5c6kw6rDVz4HGu%2Bmjv7Q%2FLjmVzFhNbeNxxjJOdCnjWUQrMRwIU3TQ4%2FmUIq4qrwI7FPS%2FkN8H0CQe6xO1UUaxUN8NdMM4n0VQDaBX9L1Sk%2F3HrN4eyqjhWr2vUXUyG%2BpsNE',
+        'XSRF-TOKEN': 'a22ef85f36bd518c68271ee0cd72d30d3BMnTujlcvm9BEZPz7jGzUl66xEa83JdNlep%2FjAv%2Bz%2BYs%2FRb%2B6biw0qtX9VaivQhvHLeYKnkXFlW1Y7eB8ucH0q05dlyvt4xDPKtAMTgOkRH5vOj3XToy1drdWggp7jg',
         '_gat': '1',
-        'session': 'a47a21556d64a6e9438080703024d0f70%2BEMjmvsElAOYJDXEabQP%2FiemN0LHRdQW4gFbhoNnKF40Mm7yL7D7myEuq7CwT3orfnUAk4GJUJxtwBMIDf76M2KsivhHbG1FCnsULyMj3wxdevY9J3evbrqfvDUQL4y',
-        'XSRF-TOKEN': '95fc6fd1e8a71870e41cc33ff4fa309eka0uDKVWhA0ickjZpsHuLhg0A%2FidsUXPgmhkNGDZhLaDRa8qOiI1kDY38UIdwl3vJ1swAHZzzyP5ltGqPhcF7OubniiaUnpn%2FTXr2aDotATByMMrNjXvON4TBlsdUdk3',
     }
 
     self.headers = {
         'authority': 'pickaw.app',
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'id-ID,id;q=0.9',
-        'if-none-match': 'W/"3ac8-VZ/y3GoEVHf/Z+TqnYsgk68AR0g"',
-        'referer': 'https://pickaw.app/contests/check',
+        'referer': 'https://pickaw.app/me/contests',
         'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -38,8 +38,8 @@ class PickawContest:
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
         'x-pickaw-version': '1.22.0',
         'x-requested-with': 'XMLHttpRequest',
-        'x-websocket-id': '',
-        'x-xsrf-token': '95fc6fd1e8a71870e41cc33ff4fa309eka0uDKVWhA0ickjZpsHuLhg0A/idsUXPgmhkNGDZhLaDRa8qOiI1kDY38UIdwl3vJ1swAHZzzyP5ltGqPhcF7OubniiaUnpn/TXr2aDotATByMMrNjXvON4TBlsdUdk3',
+        'x-websocket-id': 'YSXQD2IO-7pKgRjTAFI0',
+        'x-xsrf-token': 'a22ef85f36bd518c68271ee0cd72d30d3BMnTujlcvm9BEZPz7jGzUl66xEa83JdNlep/jAv+z+Ys/Rb+6biw0qtX9VaivQhvHLeYKnkXFlW1Y7eB8ucH0q05dlyvt4xDPKtAMTgOkRH5vOj3XToy1drdWggp7jg',
     }
 
   def request_data(self):
